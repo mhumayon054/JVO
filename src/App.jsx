8c84208eeebd68e17e6a49888d38255e82c2be00
mhumayon054 <mhumayon054@gmail.com>
2026-04-05 01:16:33 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ScrollToTop } from './components/ScrollToTop'
 import HomePage from './pages/HomePage'
 import StartupPartnershipPage from './pages/StartupPartnershipPage'
 import AboutUsPage from './pages/AboutUsPage'
@@ -11,6 +12,7 @@ import BuildSquadPage from './pages/BuildSquadPage'
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/partnership" element={<StartupPartnershipPage />} />
